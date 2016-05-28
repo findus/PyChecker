df51634279b832dfc3f275e76f14c9a3d7d9b010
Philipp <PhilippHentschel@t-online.de>
2016-05-29 01:53:28 +0200

    added hacky tray menu icon

--- PyChecker.py
+++ PyChecker.py
@@ -12,10 +12,11 @@ sys.setdefaultencoding('utf8')
 firststart = True
 dictionary = dict()
 
+
 def printsummary(json):
     notify2.init('Streams Online')
     message = ''
-    for channel in parsedJson['streams']:
+    for channel in json['streams']:
         message += " {0:20}     {1:6}\n    {2}\n\n".format(channel['channel']['display_name'],str(channel['viewers']),channel['game'])
     message = message[:-2]
     showmessage(message)
@@ -29,37 +30,47 @@ def loadFromFile():
     channels = streamfile.read().replace("\n","").split(",")
     return channels
 
+def downloadjson(channellist):
+    link = 'https://api.twitch.tv/kraken/streams/?channel=%s' % ','.join(channels)
+    #print "Connect to %s" % link
+    s = requests.get(link)
+    return json.loads(s.text)
+
+def showList():
+    json = downloadjson(channels)
+    printsummary(json)
+
+def startMainLoop():
+    while True:
+        parsedJson = downloadjson(channels)
+        global firststart
+        if firststart == True:
+            #print "First"
+            printsummary(parsedJson)
+            firststart = False
+
+        onlineoffline = ''
+        #print "Else"
+        for channel in dictionary.keys():
+            found = False
+            #print channel
+            for listchannel in parsedJson['streams']:                #print channel,'=',listchannel['channel']['name']
+                if listchannel['channel']['name'] == channel:
+                    #print "Channel Found %s , State in dict: %s" % (channel,dictionary[channel])
+                    found = True
+                    if dictionary[channel] == False:
+                        onlineoffline +=  "  %s \n" % listchannel['channel']['display_name']
+                        dictionary[channel] = True
+            #print "ended iteration, %s, %s, %s" % (found,dictionary[channel],channel)
+            if found == False and dictionary[channel] == True:
+                onlineoffline +=  "  %s \n" % listchannel['channel']['display_name']
+                dictionary[channel] = False
+        if(len(onlineoffline) > 0):
+            showmessage(onlineoffline)
+        time.sleep(60)
+
+
 channels = loadFromFile()
 
 for channelname in channels:
     dictionary[channelname] = False
-
-while True:
-    link = 'https://api.twitch.tv/kraken/streams/?channel=%s' % ','.join(channels)
-    #print "Connect to %s" % link
-    s = requests.get(link)
-    parsedJson =  json.loads(s.text)
-    if firststart == True:
-        #print "First"
-        printsummary(parsedJson)
-        firststart = False
-
-    onlineoffline = ''
-    #print "Else"
-    for channel in dictionary.keys():
-        found = False
-        #print channel
-        for listchannel in parsedJson['streams']:                #print channel,'=',listchannel['channel']['name']
-            if listchannel['channel']['name'] == channel:
-                #print "Channel Found %s , State in dict: %s" % (channel,dictionary[channel])
-                found = True
-                if dictionary[channel] == False:
-                    onlineoffline +=  "  %s \n" % listchannel['channel']['display_name']
-                    dictionary[channel] = True
-        #print "ended iteration, %s, %s, %s" % (found,dictionary[channel],channel)
-        if found == False and dictionary[channel] == True:
-            onlineoffline +=  "  %s \n" % listchannel['channel']['display_name']
-            dictionary[channel] = False
-    if(len(onlineoffline) > 0):
-        showmessage(onlineoffline)
-    time.sleep(60)
---
+++ trayicon.py
@@ -0,0 +1,55 @@
+#https://stackoverflow.com/questions/6389580/quick-and-easy-trayicon-with-python/33069455#33069455
+import wx
+import PyChecker
+from thread import start_new_thread
+
+
+TRAY_TOOLTIP = 'PyChecker'
+TRAY_ICON = 'quader.png'
+
+def create_menu_item(menu, label, func):
+    item = wx.MenuItem(menu, -1, label)
+    menu.Bind(wx.EVT_MENU, func, id=item.GetId())
+    menu.AppendItem(item)
+    return item
+
+class TaskBarIcon(wx.TaskBarIcon):
+    def __init__(self, frame):
+        self.frame = frame
+        super(TaskBarIcon, self).__init__()
+        self.set_icon(TRAY_ICON)
+        self.Bind(wx.EVT_TASKBAR_LEFT_DOWN, self.on_left_down)
+
+    def CreatePopupMenu(self):
+        menu = wx.Menu()
+        create_menu_item(menu, 'Exit', self.on_exit)
+        return menu
+
+    def set_icon(self, path):
+        icon = wx.IconFromBitmap(wx.Bitmap(path))
+        self.SetIcon(icon, TRAY_TOOLTIP)
+
+    def on_left_down(self, event):
+        print 'Tray icon was left-clicked.'
+        PyChecker.showList()
+
+    def on_exit(self, event):
+        wx.CallAfter(self.Destroy)
+        self.frame.Close()
+
+class App(wx.App):
+    def OnInit(self):
+        frame=wx.Frame(None)
+        self.SetTopWindow(frame)
+        TaskBarIcon(frame)
+        return True
+
+def main():
+    app = App(False)
+    start_new_thread(PyChecker.startMainLoop,())
+    app.MainLoop()
+
+
+
+if __name__ == '__main__':
+    main()
