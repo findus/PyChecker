1514c7965e18ebf6aa912df09cc38577af064d7c
Philipp <philipp@f1ndus.de>
2018-05-27 19:36:35 +0200

    new api impl

--- PyChecker/Twitchcheck.py
+++ PyChecker/Twitchcheck.py
@@ -14,7 +14,6 @@ sys.setdefaultencoding('utf8')
 firststart = True
 dictionary = dict()
 
-
 def printsummary(json):
     message = ''
     for channel in json['streams']:
@@ -32,6 +31,25 @@ def loadFromFile():
     streamfile = open(expanduser("~")+"/.config/.pychecker/streams")
     channels = streamfile.read().replace("\n","").split(",")
     return channels
+
+def getToken():
+    link = "https://id.twitch.itv/oauth2/authorize?client_id=hb7zvth15ub915hs3qw0h5c6xab0a1&redirect_uri=http://localhost&response_type=token&scope=analytics:read:games"
+    s = requests.get(link)
+    return s.text
+
+def getUserID():
+    link = "https://api.twitch.tv/helix/users?login=Fozruk_"
+    headers = {'Client-ID' : 'hb7zvth15ub915hs3qw0h5c6xab0a1'}
+    s = requests.get(link,headers=headers)
+    json2 = json.loads(s.text)
+    return json2["data"][0]["id"]
+
+#TODO pagination
+def getFollowedStreams():
+    link = 'https://api.twitch.tv/helix/users/follows?first=100&from_id=%s' % getUserID()
+    headers = {'Client-ID' : 'hb7zvth15ub915hs3qw0h5c6xab0a1'}
+    s = requests.get(link,headers=headers)
+    return json.loads(s.text)
 
 def downloadjson(channellist):
     link = 'https://api.twitch.tv/kraken/streams/?channel=%s' % ','.join(channels)
@@ -94,3 +112,6 @@ channels = loadFromFile()
 
 for channelname in channels:
     dictionary[channelname] = {'name' : channelname, 'online' : False , "viewers" : 0 , 'topic' : '' , 'game' : '', 'urlname' : ''}
+
+
+userID = getUserID()
